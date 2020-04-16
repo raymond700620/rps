b12af0d86eac623afaad277d38c3c856fbdaee5c
raymond700620 <rfang@pivotal.io>
2020-04-16 17:59:59 +0800

    add home getmapping

--- src/main/java/com/xp/game/rps/RPSController.java
+++ src/main/java/com/xp/game/rps/RPSController.java
@@ -1,5 +1,6 @@
 package com.xp.game.rps;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RPSController {
 
+    @GetMapping("/")
+    String hello() {
+        return "Hello!";
+    }
+    
     @PostMapping("/play")
     String play(@RequestBody PlayRequest playRequest) {
         Result result = RPS.play(Throw.valueOf(playRequest.getP1().toUpperCase()),
